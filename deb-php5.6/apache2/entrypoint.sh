a5c65c1a779bf231dfbe1407914495482cf4de00
MatheusFS <matheusfs@matheusfs-debian.matheusfs-debian>
2019-08-26 22:24:18 -0300

    .gitignore fix

--- deb-php5.6/apache2/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-update-alternatives --set php /usr/bin/php5.6
-service apache2 restart
-tail -f /tmp/dev.log
--- deb-php5.6/nginx/hub-push.sh
+++
@@ -1,3 +0,0 @@
-docker build -t matheusfs/deb-php5.6:nginx .
-docker login
-docker push matheusfs/deb-php5.6:nginx
