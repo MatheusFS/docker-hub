86b4189668398148f8f16ca1a515b56ded544f1d
Matheus Ferreira <matheusfs97@gmail.com>
2024-10-05 02:35:53 -0300

    update files

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -1,12 +1,12 @@
-SET PERSIST lower_case_table_names = 1;
+-- SET PERSIST lower_case_table_names = 1;
 
-RENAME USER 'root'@'localhost' TO 'root'@'%';
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'dipass';
+-- RENAME USER 'root'@'localhost' TO 'root'@'%';
+-- ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'dipass';
 
-CREATE USER 'exporter'@'%' IDENTIFIED BY 'dipass' WITH MAX_USER_CONNECTIONS 3;
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+-- CREATE USER 'exporter'@'%' IDENTIFIED BY 'dipass' WITH MAX_USER_CONNECTIONS 3;
+-- GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
 
-CREATE USER 'app'@'%' IDENTIFIED BY 'dipass';
-GRANT ALL PRIVILEGES ON *.* TO 'app'@'%' WITH GRANT OPTION;
+-- CREATE USER 'app'@'%' IDENTIFIED BY 'dipass';
+-- GRANT ALL PRIVILEGES ON *.* TO 'app'@'%' WITH GRANT OPTION;
 
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
