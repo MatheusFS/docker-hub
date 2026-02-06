a27e05cb5c5f54da4744546b13f4b721b1063807
MatheusFS <matheusfs97@gmail.com>
2026-02-06 02:48:21 -0300

    update fpm

--- fpm/entrypoint.sh
+++ fpm/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+
 chown -R fpm:www-data /var/www/html /home/fpm
 chmod -R 775 /var/www/html /home/fpm
 echo "Set permissions to /var/www/html and /home/fpm"
+
+# Executa o comando passado ao container
+# exec "$@"
